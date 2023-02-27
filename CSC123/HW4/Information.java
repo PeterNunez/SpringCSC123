82e65eb8ee58170be8c6d0e174fea7fe011b4a62
Pedro Nunez <peter.nunez1029@gmail.com>
2023-02-27 13:52:51 -0800

    constuctor made

---
+++ CSC123/HW4/Information.java
@@ -0,0 +1,22 @@
+
+public class Information {
+
+	private String firstname;
+	private String lastname;
+	private String SSN;
+	private String Overdraft;
+	
+	public Information(String firstname, String lastname, String SSN, String Overdraft) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.SSN = SSN;
+		this.Overdraft = Overdraft;
+	}
+	
+	
+	
+	
+	
+	
+}
