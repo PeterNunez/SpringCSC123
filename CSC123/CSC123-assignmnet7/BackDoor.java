140644f5f7f14b36986c7e1f2acebcdecacab261
Pedro Nunez <peter.nunez1029@gmail.com>
2023-05-01 21:42:52 -0700

    fixed a couple of things

--- CSC123/CSC123-assignmnet7/BackDoor.java
+++ CSC123/CSC123-assignmnet7/BackDoor.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOError;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,10 +12,10 @@ import java.net.Socket;
 
 public class BackDoor {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
-		try {
+		//try {
 			ServerSocket server=new ServerSocket(2001);
 			Socket client=server.accept();
 			String workingDir=System.getProperty("user.dir");
@@ -33,27 +34,48 @@ public class BackDoor {
 			
 			while(true) {
 				
-				writer.write(workingDir+prompt);
+				writer.write("\n\r"+workingDir+prompt);
 				writer.flush();
 				String clientCommand=reader.readLine();
 				
-				if(clientCommand.equalsIgnoreCase("pwd")) {
+				if(clientCommand.equalsIgnoreCase("cd .")) {
 					writer.write("\nWorking directory is: "+workingDir+"\n\n");
 				}
-				else if(clientCommand.equalsIgnoreCase("dir")){
-					writer.write("\nShould list files in the current directory \n\n");
+				
+				else if(clientCommand.equalsIgnoreCase("cd -")) {
+					writer.write("\nBack to the first directory is: ");
 				}
 				
-				
-			}
-			
-			
-
-		}
-		catch(IOException e) {
-			e.printStackTrace();
+				else if(clientCommand.equalsIgnoreCase("dir")){
+					writer.write("\nShould list files in the current directory \n\n");
+					File currentDirectory=new File(workingDir);
+					File[] allFiles=currentDirectory.listFiles();
+					
+					for(File f:allFiles) {
+						writer.write("\n\r"+f.getName()+(f.isDirectory()?"- Directory":"- file"));
+					}
+				}
+					else if(clientCommand.startsWith("cd"));{
+						String d =clientCommand.split(" ")[1];
+						File work;
+						
+						if(d.equalsIgnoreCase("..")) {
+							work=new File(workingDir).getParentFile();
+						}else {
+							work=new File(workingDir+File.separator+d);
+						}
+						
+						String temp=workingDir+File.separator+d;
+						if(new File(temp).isDirectory()) {
+							workingDir=temp;
+						}
+						else {
+							writer.write("\n Directory doesn't exist");
+						}
+					}
+				}
+		
 		}
 		
-	}
-
+			
 }
